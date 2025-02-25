b9475a977ee02330e31ae68fafb59e86939db10e
sanvie25 <56310223+sanvie25@users.noreply.github.com>
2025-02-25 11:22:00 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -11,15 +11,12 @@ int main(){
     int m;
     scanf("%d",&m);
     struct bonus w;
-    int sum=0;
     int i;
     for (i = 0; i<m; i++) {
         scanf("%d %s %d",&w.b[i],&w.a[i],&w.c[i]);
         }
-    // for (i = 0; i<7; i++) {
-    //     scanf("%s %d", &w.a[i],&w.b[i]);
-    //     }
-    for (i = 0; i<7; i++) {
-        printf("Employee ID:%d, Name:%s, Bonus: %f\n",&w.b[i],&w.a[i],&w.c[i]);
+
+    for (i = 0; i<m; i++) {
+        printf("Employee ID:%d, Name:%s, Bonus: %d\n",&w.b[i],&w.a[i],&w.c[i]);
         }
 }
