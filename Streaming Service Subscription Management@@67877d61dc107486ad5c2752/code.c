4d629d3449e8ac0d9e1d5a90a6307f487f88a951
sanvie25 <56310223+sanvie25@users.noreply.github.com>
2025-03-02 13:22:55 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -7,34 +7,35 @@ struct subscription {
     float c[25]; 
 };
 
-int main(){
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     struct subscription q;
     int i;
-    int b=0;
-    float br=0.00,pr=0.00,sr=0.00;
-    int p=0;
-    int s=0;
+    int b = 0, p = 0, s = 0;
+    float br = 0.00, pr = 0.00, sr = 0.00;
 
     for (i = 0; i < n; i++) {
-        scanf("%s %s %.1f", &q.a[i], q.b[i], &q.c[i]);
-        if(q.b[i]=="Basic"){
-            b+=1;
-            br+=199.00;
-        }
-        else if(q.b[i]=="Standard"){
-            s+=1;
-            sr+=499.00;
-        }
-        else{
-            p+=1;
-            pr+=999.00;
+        scanf("%s %s %f", q.a[i], q.b[i], &q.c[i]);
+
+        if (strcmp(q.b[i], "Basic") == 0) {
+            b += 1;
+            br += 199.00;
+        } 
+        else if (strcmp(q.b[i], "Standard") == 0) {
+            s += 1;
+            sr += 499.00;
+        } 
+        else {
+            p += 1;
+            pr += 999.00;
         }
     }
 
-    printf("Basic: %d Users, Revenue: %d; Standard: %d Users, Revenue: %d; Premium: %d Users, Revenue: %d;",b,br,s,sr,p,pr);
+    printf("Basic: %d Users, Revenue: %.2f; ", b, br);
+    printf("Standard: %d Users, Revenue: %.2f; ", s, sr);
+    printf("Premium: %d Users, Revenue: %.2f;\n", p, pr);
 
     return 0;
 }
