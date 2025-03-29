f317ce374c0546d2c6d07b291747c827f885b157
sanvie25 <56310223+sanvie25@users.noreply.github.com>
2025-03-29 17:52:03 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -26,23 +26,3 @@ void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
     }
 }
 
-int main() {
-    int n, m;
-    scanf("%d", &n);
-    int ranked[200000];
-    for (int i = 0; i < n; i++) scanf("%d", &ranked[i]);
-
-    scanf("%d", &m);
-    int player[200000];
-    for (int i = 0; i < m; i++) scanf("%d", &player[i]);
-
-    int result[200000];
-
-    // Call function
-    trackPlayerRanks(ranked, n, player, m, result);
-
-    // Print results
-    for (int i = 0; i < m; i++) printf("%d\n", result[i]);
-
-    return 0;
-}
